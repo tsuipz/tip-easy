3b116c19ade0d20e1eb7bd2ed534844fabacec50
tsuipz <tsuipz@gmail.com>
2022-01-18 22:57:03 -0500

    Updated styling

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,9 +3,8 @@ import classes from './Input.module.css';
 const Input = () => {
 	return (
 		<div className={classes.input}>
-			<label>
-				Total Price: <input type='text' placeholder='Enter Total Price' required />
-			</label>
+			<label>Total Price:</label>
+			<input type='text' placeholder='Enter Total Price' required />
 		</div>
 	);
 };
